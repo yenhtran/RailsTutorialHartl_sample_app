4514cb0f90ac7d5bd5ea483ea1c8fd0b2fc2feba
Yen Tran <yentran@Yens-MacBook-Pro-4.local>
2014-12-12 10:44:14 -0800

    Add User resources 	-Add Gravatar 	-Handle Error Validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+  	@user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(strong_params)
+  	if @user.save
+  		# success
+  	else 
+  		render 'new'
+  	end
+  end
+
+  private
+
+	  def strong_params
+	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  end
+
 end
